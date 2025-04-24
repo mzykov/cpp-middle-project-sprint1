340b823401088bf223167e912a752d8c33aa02d4
Mikhail Zykov <zmsmihail@yandex.ru>
2025-04-25 02:41:25 +0300

    add main scenario test

--- tests/crypto_guard_ctx_test.cpp
+++ tests/crypto_guard_ctx_test.cpp
@@ -1,3 +1,42 @@
+#include "crypto_guard_ctx.h"
 #include <gtest/gtest.h>
+#include <iostream>
+#include <sstream>
 
-TEST(TestComponentName, SimpleCheck) { EXPECT_EQ(1 + 1, 2); }
+TEST(TestCryptoGuardCtx, TestMainScenario) {
+    // given
+    std::string givenStr = "My private content to be encrypted\n", password = "My robust password";
+    std::string checksumOfGivenStr, encryptedStr, decryptedStr, checksumOfDecryptedStr;
+
+    // when
+    {
+        std::stringstream inSStream;
+        inSStream << givenStr;
+        CryptoGuard::CryptoGuardCtx ctx;
+        checksumOfGivenStr = ctx.CalculateChecksum(inSStream);
+    }
+    {
+        std::stringstream inSStream, outSStream;
+        inSStream << givenStr;
+        CryptoGuard::CryptoGuardCtx ctx;
+        ctx.EncryptFile(inSStream, outSStream, password);
+        encryptedStr = outSStream.str();
+    }
+    {
+        std::stringstream inSStream, outSStream;
+        inSStream << encryptedStr;
+        CryptoGuard::CryptoGuardCtx ctx;
+        ctx.DecryptFile(inSStream, outSStream, password);
+        decryptedStr = outSStream.str();
+    }
+    {
+        std::stringstream inSStream;
+        inSStream << decryptedStr;
+        CryptoGuard::CryptoGuardCtx ctx;
+        checksumOfDecryptedStr = ctx.CalculateChecksum(inSStream);
+    }
+
+    // then
+    EXPECT_EQ(givenStr, decryptedStr);
+    EXPECT_EQ(checksumOfGivenStr, checksumOfDecryptedStr);
+}
