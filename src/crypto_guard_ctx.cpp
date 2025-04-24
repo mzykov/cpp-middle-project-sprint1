8c323c658684b5ca8c09e9a433544a8999450f84
Mikhail Zykov <zmsmihail@yandex.ru>
2025-04-25 01:24:44 +0300

    fix error handling

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -2,6 +2,7 @@
 #include <array>
 #include <cstddef>
 #include <iomanip>
+#include <openssl/err.h>
 #include <openssl/evp.h>
 #include <stdexcept>
 #include <vector>
@@ -16,19 +17,6 @@ struct AesCipherParams {
     std::array<unsigned char, IV_SIZE> iv;         // Initialization vector
 };
 
-AesCipherParams CreateCipherParamsFromPassword(std::string_view password) {
-    AesCipherParams params;
-    constexpr std::array<unsigned char, 8> salt = {'M', 'i', 'k', 'h', 'a', 'i', 'l', '.'};
-
-    int ok = EVP_BytesToKey(params.cipher, EVP_sha256(), salt.data(),
-                            reinterpret_cast<const unsigned char *>(password.data()), password.size(), 1,
-                            params.key.data(), params.iv.data());
-    if (!ok) {
-        throw std::runtime_error{"Failed to create a key from password"};
-    }
-    return params;
-}
-
 using evp_cipher_unique_ptr =
     std::unique_ptr<EVP_CIPHER_CTX, decltype([](EVP_CIPHER_CTX *ctx) { EVP_CIPHER_CTX_free(ctx); })>;
 
@@ -36,9 +24,15 @@ using evp_md_unique_ptr = std::unique_ptr<EVP_MD_CTX, decltype([](EVP_MD_CTX *md
 
 class CryptoGuardCtx::Impl {
 public:
-    Impl() { OpenSSL_add_all_algorithms(); }
+    Impl() {
+        OpenSSL_add_all_algorithms();
+        ERR_load_crypto_strings();
+    }
 
-    ~Impl() { EVP_cleanup(); }
+    ~Impl() {
+        EVP_cleanup();
+        ERR_free_strings();
+    }
     // API
     void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password);
     void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password);
@@ -47,22 +41,48 @@ public:
 private:
     enum class CIPHER_ACTION { DECRYPT, ENCRYPT };
 
+    AesCipherParams createCipherParamsFromPassword(std::string_view password);
+
     evp_cipher_unique_ptr createCipherCtx(std::string_view password, int whatToDo);
     std::string getCipherChunk(std::iostream &in, EVP_CIPHER_CTX *ctx);
 
     evp_md_unique_ptr createMessageDigestCtx(std::string_view digestName);
     std::string getMmessageDigest(std::iostream &inStream, EVP_MD_CTX *mdctx);
+
+    std::exception runtimeCryptoGuardCtxException(std::string_view msg);
 };
+
+std::exception CryptoGuardCtx::Impl::runtimeCryptoGuardCtxException(std::string_view msg) {
+    std::string err;
+    err += msg;
+    err += "\n";
+    err += ERR_error_string(ERR_get_error(), nullptr);
+    return std::move(std::runtime_error(err));
+}
+
+AesCipherParams CryptoGuardCtx::Impl::createCipherParamsFromPassword(std::string_view password) {
+    AesCipherParams params;
+    constexpr std::array<unsigned char, 8> salt = {'M', 'i', 'k', 'h', 'a', 'i', 'l', '.'};
+
+    int ok = EVP_BytesToKey(params.cipher, EVP_sha256(), salt.data(),
+                            reinterpret_cast<const unsigned char *>(password.data()), password.size(), 1,
+                            params.key.data(), params.iv.data());
+    if (!ok) {
+        throw runtimeCryptoGuardCtxException("Failed to create a key from password");
+    }
+
+    return params;
+}
 
 evp_cipher_unique_ptr CryptoGuardCtx::Impl::createCipherCtx(std::string_view password, int whatToDo) {
     auto ctx = evp_cipher_unique_ptr(EVP_CIPHER_CTX_new());
 
     if (!ctx) {
-        throw std::runtime_error{"EVP_CIPHER_CTX_new failed"};
+        throw runtimeCryptoGuardCtxException("EVP_CIPHER_CTX_new failed");
     }
 
     if (password.length() > 0) {
-        auto params = CreateCipherParamsFromPassword(password);
+        auto params = createCipherParamsFromPassword(password);
         EVP_CipherInit_ex(ctx.get(), params.cipher, nullptr, params.key.data(), params.iv.data(), whatToDo);
     }
 
@@ -83,7 +103,7 @@ std::string CryptoGuardCtx::Impl::getCipherChunk(std::iostream &inStream, EVP_CI
                               inputLen);
 
     if (!ok) {
-        throw std::runtime_error{"EVP_EncryptUpdate failed"};
+        throw runtimeCryptoGuardCtxException("EVP_CipherUpdate failed");
     }
 
     std::string cipherChunk;
@@ -118,17 +138,17 @@ void CryptoGuardCtx::Impl::DecryptFile(std::iostream &inStream, std::iostream &o
 evp_md_unique_ptr CryptoGuardCtx::Impl::createMessageDigestCtx(std::string_view digestName) {
     const auto md = EVP_get_digestbyname(digestName.data());
     if (!md) {
-        throw std::runtime_error{"EVP_get_digestbyname failed"};
+        throw runtimeCryptoGuardCtxException("EVP_get_digestbyname failed");
     }
 
     auto mdctx = evp_md_unique_ptr(EVP_MD_CTX_new());
     if (!mdctx) {
-        throw std::runtime_error{"EVP_MD_CTX_new failed"};
+        throw runtimeCryptoGuardCtxException("EVP_MD_CTX_new failed");
     }
 
     int ok = EVP_DigestInit_ex2(mdctx.get(), md, nullptr);
     if (!ok) {
-        throw std::runtime_error{"EVP_DigestInit_ex2 failed"};
+        throw runtimeCryptoGuardCtxException("EVP_DigestInit_ex2 failed");
     }
 
     return std::move(mdctx);
@@ -145,7 +165,7 @@ std::string CryptoGuardCtx::Impl::getMmessageDigest(std::iostream &inStream, EVP
         int ok = EVP_DigestUpdate(mdctx, chunk.data(), inputLen);
 
         if (!ok) {
-            throw std::runtime_error{"EVP_DigestUpdate failed"};
+            throw runtimeCryptoGuardCtxException("EVP_DigestUpdate failed");
         }
     }
 
@@ -155,7 +175,7 @@ std::string CryptoGuardCtx::Impl::getMmessageDigest(std::iostream &inStream, EVP
     int ok = EVP_DigestFinal_ex(mdctx, mdValue.data(), &mdLen);
 
     if (!ok) {
-        throw std::runtime_error{"EVP_DigestFinal_ex failed"};
+        throw runtimeCryptoGuardCtxException("EVP_DigestFinal_ex failed");
     }
 
     std::stringstream ss;
