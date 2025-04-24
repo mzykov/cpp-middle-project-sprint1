3ce484baeeccdcbbb1ceb3e28106433b83bfcba7
Mikhail Zykov <zmsmihail@yandex.ru>
2025-04-24 23:29:04 +0300

    checksum done

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -1,7 +1,7 @@
 #include "crypto_guard_ctx.h"
 #include <array>
 #include <cstddef>
-#include <iostream>
+#include <iomanip>
 #include <openssl/evp.h>
 #include <stdexcept>
 #include <vector>
@@ -32,20 +32,26 @@ AesCipherParams CreateCipherParamsFromPassword(std::string_view password) {
 using evp_cipher_unique_ptr =
     std::unique_ptr<EVP_CIPHER_CTX, decltype([](EVP_CIPHER_CTX *ctx) { EVP_CIPHER_CTX_free(ctx); })>;
 
+using evp_md_unique_ptr = std::unique_ptr<EVP_MD_CTX, decltype([](EVP_MD_CTX *mdctx) { EVP_MD_CTX_free(mdctx); })>;
+
 class CryptoGuardCtx::Impl {
 public:
     Impl() { OpenSSL_add_all_algorithms(); }
 
     ~Impl() { EVP_cleanup(); }
     // API
-    void EncryptFile(std::iostream &in, std::iostream &out, std::string_view password);
-    void DecryptFile(std::iostream &in, std::iostream &out, std::string_view password);
-    std::string CalculateChecksum(std::iostream &in) { return "LET'S IMPLEMENT IT!"; }
+    void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password);
+    void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password);
+    std::string CalculateChecksum(std::iostream &inStream);
 
 private:
     enum class CIPHER_ACTION { DECRYPT, ENCRYPT };
+
     evp_cipher_unique_ptr createCipherCtx(std::string_view password, int whatToDo);
     std::string getCipherChunk(std::iostream &in, EVP_CIPHER_CTX *ctx);
+
+    evp_md_unique_ptr createMessageDigestCtx(std::string_view digestName);
+    std::string getMmessageDigest(std::iostream &inStream, EVP_MD_CTX *mdctx);
 };
 
 evp_cipher_unique_ptr CryptoGuardCtx::Impl::createCipherCtx(std::string_view password, int whatToDo) {
@@ -107,6 +113,63 @@ void CryptoGuardCtx::Impl::DecryptFile(std::iostream &inStream, std::iostream &o
         const auto chunk = getCipherChunk(inStream, ctx.get());
         outStream << chunk;
     }
+}
+
+evp_md_unique_ptr CryptoGuardCtx::Impl::createMessageDigestCtx(std::string_view digestName) {
+    const auto md = EVP_get_digestbyname(digestName.data());
+    if (!md) {
+        throw std::runtime_error{"EVP_get_digestbyname failed"};
+    }
+
+    auto mdctx = evp_md_unique_ptr(EVP_MD_CTX_new());
+    if (!mdctx) {
+        throw std::runtime_error{"EVP_MD_CTX_new failed"};
+    }
+
+    int ok = EVP_DigestInit_ex2(mdctx.get(), md, nullptr);
+    if (!ok) {
+        throw std::runtime_error{"EVP_DigestInit_ex2 failed"};
+    }
+
+    return std::move(mdctx);
+}
+
+std::string CryptoGuardCtx::Impl::getMmessageDigest(std::iostream &inStream, EVP_MD_CTX *mdctx) {
+    constexpr int chunkSizeInBytes = 1024;
+
+    while (!inStream.eof()) {
+        std::vector<char> chunk(chunkSizeInBytes, 0);
+        inStream.read(chunk.data(), chunk.size());
+        std::streamsize inputLen = inStream.gcount();
+
+        int ok = EVP_DigestUpdate(mdctx, chunk.data(), inputLen);
+
+        if (!ok) {
+            throw std::runtime_error{"EVP_DigestUpdate failed"};
+        }
+    }
+
+    std::array<unsigned char, EVP_MAX_MD_SIZE> mdValue;
+    unsigned int mdLen;
+
+    int ok = EVP_DigestFinal_ex(mdctx, mdValue.data(), &mdLen);
+
+    if (!ok) {
+        throw std::runtime_error{"EVP_DigestFinal_ex failed"};
+    }
+
+    std::stringstream ss;
+
+    for (int i = 0; i < mdLen; ++i)
+        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(mdValue[i]);
+
+    return ss.str();
+}
+
+std::string CryptoGuardCtx::Impl::CalculateChecksum(std::iostream &inStream) {
+    const std::string digestName = "SHA256";
+    auto mdctx = createMessageDigestCtx(digestName);
+    return getMmessageDigest(inStream, mdctx.get());
 }
 
 CryptoGuardCtx::CryptoGuardCtx() : pImpl_(std::make_unique<Impl>()) {}
