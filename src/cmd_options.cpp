682dc8290aef92ee7a77a21478e78975d3ce64c7
Mikhail Zykov <zmsmihail@yandex.ru>
2025-04-28 23:47:01 +0300

    review fixes: tune cmd options

--- src/cmd_options.cpp
+++ src/cmd_options.cpp
@@ -1,27 +1,40 @@
 #include "cmd_options.h"
+#include <array>
 #include <utility>
 
 namespace CryptoGuard {
 
-ProgramOptions::ProgramOptions() : desc_("Allowed options") {
+ProgramOptions::ProgramOptions() : desc_("Allowed options"), command_(ProgramOptions::COMMAND_TYPE::HELP) {
+
     namespace po = boost::program_options;
-    desc_.add_options()("help", "print help message")("command", po::value<std::string>(),
-                                                      "command to execute: encrypt/decrypt/checksum")(
-        "input", po::value<std::string>(), "input file path")("output", po::value<std::string>(), "output file path")(
-        "password", po::value<std::string>(), "owner password");
+    desc_.add_options()("help, h", "print help message")("command, c", po::value<std::string>(),
+                                                         "command to execute: encrypt/decrypt/checksum")(
+        "input, i", po::value<std::string>(),
+        "input file path")("output, o", po::value<std::string>(),
+                           "output file path")("password, p", po::value<std::string>(), "owner password");
 }
 
 bool ProgramOptions::Parse(int ac, char *av[]) {
     auto vm = parseCommandLine(ac, av);
 
+    if (vm.contains("help")) {
+        return true;
+    }
     if (!optionsAreConsistent(vm)) {
         return false;
     }
-
-    setCommand(vm);
-    setInputFile(vm);
-    setOutputFile(vm);
-    setPassword(vm);
+    if (vm.contains("command")) {
+        command_ = commandMapping_[vm["command"].as<std::string>()];
+    }
+    if (vm.contains("input")) {
+        inputFile_ = vm["input"].as<std::string>();
+    }
+    if (vm.contains("output")) {
+        outputFile_ = vm["output"].as<std::string>();
+    }
+    if (vm.contains("password")) {
+        password_ = vm["password"].as<std::string>();
+    }
 
     return true;
 }
@@ -36,33 +49,30 @@ boost::program_options::variables_map ProgramOptions::parseCommandLine(int ac, c
 }
 
 bool ProgramOptions::optionsAreConsistent(const boost::program_options::variables_map &vm) const {
-    if (vm.count("help")) {
-        return true;
-    } else if (vm.count("command") == 1) {
-        const auto &cmd = vm["command"].as<std::string>();
-
-        if (!commandMapping_.count(cmd)) {
-            return false;
-        }
-
-        std::unordered_map<ProgramOptions::COMMAND_TYPE, std::vector<std::pair<std::string, bool>>> consistensyMapping =
-            {
-                {ProgramOptions::COMMAND_TYPE::CHECKSUM, {{"input", true}, {"output", false}, {"password", false}}},
-                {ProgramOptions::COMMAND_TYPE::DECRYPT, {{"input", true}, {"output", true}, {"password", true}}},
-                {ProgramOptions::COMMAND_TYPE::ENCRYPT, {{"input", true}, {"output", true}, {"password", true}}},
-            };
-
-        const auto cmdType = commandMapping_.at(cmd);
-
-        for (const auto &[opt, musthave] : consistensyMapping[cmdType]) {
-            if (!ProgramOptions::optionIsConsistent(vm, opt, musthave))
-                return false;
-        }
-
-        return true;
+    if (!vm.count("command")) {
+        return false;
     }
 
-    return false;
+    const auto &cmd = vm["command"].as<std::string>();
+
+    if (!commandMapping_.count(cmd)) {
+        return false;
+    }
+
+    std::unordered_map<ProgramOptions::COMMAND_TYPE, std::array<std::pair<std::string, bool>, 3>> consistensyMapping = {
+        {ProgramOptions::COMMAND_TYPE::CHECKSUM, {{{"input", true}, {"output", false}, {"password", false}}}},
+        {ProgramOptions::COMMAND_TYPE::DECRYPT, {{{"input", true}, {"output", true}, {"password", true}}}},
+        {ProgramOptions::COMMAND_TYPE::ENCRYPT, {{{"input", true}, {"output", true}, {"password", true}}}},
+    };
+
+    const auto cmdType = commandMapping_.at(cmd);
+
+    for (const auto &[opt, musthave] : consistensyMapping[cmdType]) {
+        if (!ProgramOptions::optionIsConsistent(vm, opt, musthave))
+            return false;
+    }
+
+    return true;
 }
 
 bool ProgramOptions::optionIsConsistent(const boost::program_options::variables_map &vm, const std::string &opt,
@@ -78,32 +88,6 @@ bool ProgramOptions::optionIsConsistent(const boost::program_options::variables_
     }
 
     return true;
-}
-
-void ProgramOptions::setCommand(const boost::program_options::variables_map &vm) {
-    if (vm.count("help")) {
-        command_ = commandMapping_["help"];
-    } else {
-        command_ = commandMapping_[vm["command"].as<std::string>()];
-    }
-}
-
-void ProgramOptions::setInputFile(const boost::program_options::variables_map &vm) {
-    if (vm.count("input")) {
-        inputFile_ = vm["input"].as<std::string>();
-    }
-}
-
-void ProgramOptions::setOutputFile(const boost::program_options::variables_map &vm) {
-    if (vm.count("output")) {
-        outputFile_ = vm["output"].as<std::string>();
-    }
-}
-
-void ProgramOptions::setPassword(const boost::program_options::variables_map &vm) {
-    if (vm.count("password")) {
-        password_ = vm["password"].as<std::string>();
-    }
 }
 
 }  // namespace CryptoGuard
